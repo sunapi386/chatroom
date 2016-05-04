6ab1989ba7ce2c39bb059b84c0d8c245bd86a337
Jason Sun <j_sun@apple.com>
2016-05-03 17:14:59 -0700

    cors allow http://localhost:4200

--- chatapi/config/environments/development.rb
+++ chatapi/config/environments/development.rb
@@ -45,3 +45,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:4200']
